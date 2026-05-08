306e970650cab06acfe4a166feffd8d151ec8ba9
Juri Santini <santini.juri@franchettisalviani.net>
2026-05-08 10:02:44 +0200

    Creazione classe DBConnection

---
+++ TombollaSantiniIonescuGraziotti/src/tombollasantiniionescugraziotti/DBConnection.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tombollasantiniionescugraziotti;
+
+/**
+ *
+ * @author santini.juri
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/tombola";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("Driver MySQL non trovato", e);
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+}
